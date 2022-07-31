a5c7ee4bb479e12beb26ab31917473072a749f46
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-07-31 14:36:18 +0900

    Added feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#[cfg(feature = "aria_attributes")]
 pub mod aria_attributes;
+
+#[cfg(feature = "attribute_collection")]
 pub mod attribute_collection;
+
+#[cfg(feature = "misc_attributes")]
 pub mod misc_attributes;
